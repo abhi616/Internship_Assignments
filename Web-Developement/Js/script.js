81d0176ed0022aab9105efac560b29b01a38feae
abhi616 <ay891241@gmail.com>
2022-03-15 19:20:38 +0530

    Website

--- Web-Developement/Js/script.js
+++ Web-Developement/Js/script.js
@@ -1,4 +1,10 @@
 function getData(){
-    const name = document.getElementById['name'];
-    alert("Name : "+name);
+    const name = document.getElementById("name").value;
+    const clg = document.getElementById("college").value;
+    const mob = document.getElementById("mobileno").value;
+    const email = document.getElementById("email").value;
+    alert(`Name : ${name} 
+College : ${clg} 
+Mobile Number : ${mob} 
+Email ID : ${email}`);
 }
