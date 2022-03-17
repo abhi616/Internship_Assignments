fb52e19ac9c753a7194694acac6fbc18d8802709
abhi616 <ay891241@gmail.com>
2022-03-17 22:55:20 +0530

    Login_Form

---
+++ Project/Login/Js/script.js
@@ -0,0 +1,7 @@
+function login(){
+    const user = document.getElementById("user").value;
+    const pass = document.getElementById("pass").value;
+    alert(`Login Successful.
+Username : ${user}
+Password : ${pass}`);
+}
