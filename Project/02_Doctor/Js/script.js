f92b3d24e4de288b4c483e563f528a1ac114e17b
abhi616 <ay891241@gmail.com>
2022-03-21 22:16:47 +0530

    Doctor_Module

---
+++ Project/02_Doctor/Js/script.js
@@ -0,0 +1,46 @@
+function getData(){
+    const id = document.getElementById('Did').value;
+    const fname = document.getElementById('DfName').value;
+    const mname = document.getElementById('DmName').value;
+    const surname = document.getElementById('DSurName').value;
+    const fullname = fname+" "+mname+" "+surname;
+    const dob = document.getElementById('Ddob').value;
+    const age = document.getElementById('DAge').value;
+    const gen = document.getElementById('Gen').value;
+    const education = document.getElementById('DEducation').value;
+    const mob = document.getElementById('mob').value;
+    const alternateno = document.getElementById('alternatemob').value;
+    const email = document.getElementById('email').value;
+    const aadhar = document.getElementById('aadhaar').value;
+
+    alert(`Doctor Registered Successfully...
+
+Doctor ID : ${id}
+Doctor Name : ${fullname}
+DOB : ${dob}
+Age : ${age}
+Gender : ${gen}
+Education : ${education}
+Mobile Number : ${mob}
+Alternate Number : ${alternateno}
+Email ID : ${email}
+Aadhaar Number : ${aadhar}`
+);
+
+}
+
+function clrData(){
+    document.getElementById('Did').value = "";
+    document.getElementById('DfName').value = "";
+    document.getElementById('DmName').value = "";
+    document.getElementById('DSurName').value = "";
+    document.getElementById('Ddob').value = "";
+    document.getElementById('DAge').value = "";
+    document.getElementById('Gen').value = "";
+    document.getElementById('DEducation').value = "";
+    document.getElementById('mob').value = "";
+    document.getElementById('alternatemob').value = "";
+    document.getElementById('email').value = "";
+    document.getElementById('aadhaar').value = "";
+    document.getElementById('Did').focus(); 
+}
